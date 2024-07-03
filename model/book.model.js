dfd0f6c57af5a2af01e76d7a53d19df7b418719d
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-07-03 09:16:00 +0000

    added

--- model/book.model.js
+++ model/book.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
-    name: String,
-    price: Number,
-    category: String,
-    image: String,
-    title: String,
-});
+    bookId: { type: String, required: true },
+    bookTitle: { type: String, required: true },
+    createdBy: { type: String, required: true },
+    createdTime: { type: Date, default: Date.now },
+    image: { type: String, required: true },
+    pdfLink: { type: String, required: true },
+    semester: { type: String, required: true } // Added semester field
+  },);
 const Book = mongoose.model("Book", bookSchema);
 
 export default Book;
